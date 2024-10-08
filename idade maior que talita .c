c5e10fa482a8283b0af2cf7f64284c2f7b201cce
Talita Mota <tmotarocha74@gmail.com>
2024-10-08 09:33:43 -0300

    Add files via upload

---
+++ idade maior que talita .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+int idadeTalita = 16;
+int idadePamella = 18; 
+
+if(idadeTalita < idadePamella){
+    printf("Pamella e mais velha que Talita!");
+}else {
+    printf("Talita e mais velha que Pamella!");
+}
+
+}
